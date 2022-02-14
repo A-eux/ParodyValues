bcd7d98f8234c2c5166c36a4b94387f6f3cc1b15
Bat Gamer <83028621+A-eux@users.noreply.github.com>
2022-02-13 19:56:26 -0500

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -1,429 +1,429 @@
 ideologies = [
     {
-        "name": "Anarcho-Communism",
+        "name": "Anarckiddy",
         "stats": {
             "econ": 100,
-            "dipl": 50,
+            "dipl": 90,
             "govt": 100,
-            "scty": 90
-        }
-    },
-    {
-        "name": "Libertarian Communism",
-        "stats": {
-            "econ": 100,
-            "dipl": 70,
-            "govt": 80,
-            "scty": 80
-        }
-    },
-    {
-        "name": "Trotskyism",
-        "stats": {
-            "econ": 100,
-            "dipl": 100,
-            "govt": 60,
-            "scty": 80
-        }
-    },
-    {
-        "name": "Marxism",
-        "stats": {
-            "econ": 100,
-            "dipl": 70,
-            "govt": 40,
-            "scty": 80
-        }
-    },
-    {
-        "name": "De Leonism",
-        "stats": {
-            "econ": 100,
-            "dipl": 30,
-            "govt": 30,
-            "scty": 80
-        }
-    },
-    {
-        "name": "Leninism",
-        "stats": {
-            "econ": 100,
-            "dipl": 40,
-            "govt": 20,
-            "scty": 70
-        }
-    },
-    {
-        "name": "Stalinism/Maoism",
-        "stats": {
-            "econ": 100,
-            "dipl": 20,
-            "govt": 0,
-            "scty": 60
-        }
-    },
-    {
-        "name": "Religious Communism",
-        "stats": {
-            "econ": 100,
-            "dipl": 50,
-            "govt": 30,
-            "scty": 30
-        }
-    },
-    {
-        "name": "State Socialism",
-        "stats": {
-            "econ": 80,
-            "dipl": 30,
-            "govt": 30,
-            "scty": 70
-        }
-    },
-    {
-        "name": "Theocratic Socialism",
-        "stats": {
-            "econ": 80,
-            "dipl": 50,
-            "govt": 30,
-            "scty": 20
-        }
-    },
-    {
-        "name": "Religious Socialism",
-        "stats": {
-            "econ": 80,
-            "dipl": 50,
-            "govt": 70,
-            "scty": 20
-        }
-    },
-    {
-        "name": "Democratic Socialism",
-        "stats": {
-            "econ": 80,
-            "dipl": 50,
-            "govt": 50,
-            "scty": 80
-        }
-    },
-    {
-        "name": "Revolutionary Socialism",
-        "stats": {
-            "econ": 80,
-            "dipl": 20,
-            "govt": 50,
-            "scty": 70
-        }
-    },
-    {
-        "name": "Libertarian Socialism",
-        "stats": {
-            "econ": 80,
-            "dipl": 80,
-            "govt": 80,
-            "scty": 80
-        }
-    },
-    {
-        "name": "Anarcho-Syndicalism",
-        "stats": {
-            "econ": 80,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 80
-        }
-    },
-    {
-        "name": "Left-Wing Populism",
-        "stats": {
-            "econ": 60,
-            "dipl": 40,
-            "govt": 30,
-            "scty": 70
-        }
-    },
-    {
-        "name": "Theocratic Distributism",
-        "stats": {
-            "econ": 60,
-            "dipl": 40,
-            "govt": 30,
-            "scty": 20
-        }
-    },
-    {
-        "name": "Distributism",
-        "stats": {
-            "econ": 60,
-            "dipl": 50,
-            "govt": 50,
-            "scty": 20
-        }
-    },
-    {
-        "name": "Social Liberalism",
-        "stats": {
-            "econ": 60,
-            "dipl": 60,
-            "govt": 60,
-            "scty": 80
-        }
-    },
-    {
-        "name": "Christian Democracy",
-        "stats": {
-            "econ": 60,
-            "dipl": 60,
-            "govt": 40,
-            "scty": 30
-        }
-    },
-    {
-        "name": "Social Democracy",
-        "stats": {
-            "econ": 60,
-            "dipl": 70,
-            "govt": 40,
-            "scty": 80
-        }
-    },
-    {
-        "name": "Progressivism",
-        "stats": {
-            "econ": 60,
-            "dipl": 80,
-            "govt": 60,
             "scty": 100
         }
     },
     {
-        "name": "Anarcho-Mutualism",
+        "name": "Theroy Nerd",
         "stats": {
-            "econ": 60,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 70
-        }
-    },
-    {
-        "name": "National Totalitarianism",
-        "stats": {
-            "econ": 50,
-            "dipl": 20,
-            "govt": 0,
-            "scty": 50
-        }
-    },
-    {
-        "name": "Global Totalitarianism",
-        "stats": {
-            "econ": 50,
-            "dipl": 80,
-            "govt": 0,
-            "scty": 50
-        }
-    },
-    {
-        "name": "Technocracy",
-        "stats": {
-            "econ": 60,
-            "dipl": 60,
-            "govt": 20,
-            "scty": 70
-        }
-    },
-    {
-        "name": "Centrist",
-        "stats": {
-            "econ": 50,
-            "dipl": 50,
-            "govt": 50,
-            "scty": 50
-        }
-    },
-    {
-        "name": "Liberalism",
-        "stats": {
-            "econ": 50,
-            "dipl": 60,
-            "govt": 60,
-            "scty": 60
-        }
-    },
-    {
-        "name": "Religious Anarchism",
-        "stats": {
-            "econ": 50,
-            "dipl": 50,
-            "govt": 0,
-            "scty": 20
-        }
-    },
-    {
-        "name": "Right-Wing Populism",
-        "stats": {
-            "econ": 40,
-            "dipl": 30,
-            "govt": 30,
-            "scty": 30
-        }
-    },
-    {
-        "name": "Moderate Conservatism",
-        "stats": {
-            "econ": 40,
-            "dipl": 40,
-            "govt": 50,
-            "scty": 30
-        }
-    },
-    {
-        "name": "Reactionary",
-        "stats": {
-            "econ": 40,
-            "dipl": 40,
+            "econ": 100,
+            "dipl": 100,
             "govt": 40,
-            "scty": 10
-        }
-    },
-    {
-        "name": "Social Libertarianism",
-        "stats": {
-            "econ": 60,
-            "dipl": 70,
-            "govt": 80,
-            "scty": 70
-        }
-    },
-    {
-        "name": "Libertarianism",
-        "stats": {
-            "econ": 40,
-            "dipl": 60,
-            "govt": 80,
-            "scty": 60
-        }
-    },
-    {
-        "name": "Anarcho-Egoism",
-        "stats": {
-            "econ": 40,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 50
-        }
-    },
-    {
-        "name": "Nazism",
-        "stats": {
-            "econ": 40,
-            "dipl": 0,
-            "govt": 0,
-            "scty": 5
-        }
-    },
-    {
-        "name": "Autocracy",
-        "stats": {
-            "econ": 50,
-            "dipl": 20,
-            "govt": 20,
-            "scty": 50
-        }
-    },
-    {
-        "name": "Fascism",
-        "stats": {
-            "econ": 40,
-            "dipl": 20,
-            "govt": 20,
-            "scty": 20
-        }
-    },
-    {
-        "name": "Capitalist Fascism",
-        "stats": {
-            "econ": 20,
-            "dipl": 20,
-            "govt": 20,
-            "scty": 20
-        }
-    },
-    {
-        "name": "Conservatism",
-        "stats": {
-            "econ": 30,
-            "dipl": 40,
-            "govt": 40,
-            "scty": 20
-        }
-    },
-    {
-        "name": "Neo-Liberalism",
-        "stats": {
-            "econ": 30,
-            "dipl": 30,
-            "govt": 50,
-            "scty": 60
-        }
-    },
-    {
-        "name": "Classical Liberalism",
-        "stats": {
-            "econ": 30,
-            "dipl": 60,
-            "govt": 60,
             "scty": 80
         }
     },
     {
-        "name": "Authoritarian Capitalism",
+        "name": "Icepicked",
         "stats": {
-            "econ": 20,
-            "dipl": 30,
-            "govt": 20,
-            "scty": 40
+            "econ": 100,
+            "dipl": 100,
+            "govt": 30,
+            "scty": 60
         }
     },
     {
-        "name": "State Capitalism",
+        "name": "Commie",
         "stats": {
-            "econ": 20,
+            "econ": 100,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Kaiserreich Enjoier",
+        "stats": {
+            "econ": 80,
+            "dipl": 30,
+            "govt": 70,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Tankie",
+        "stats": {
+            "econ": 70,
+            "dipl": 50,
+            "govt": 0,
+            "scty": 80
+        }
+    },
+    {
+        "name": "NAZBOL GANG",
+        "stats": {
+            "econ": 80,
+            "dipl": 0,
+            "govt": 20,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Worker Jihad",
+        "stats": {
+            "econ": 100,
+            "dipl": 100,
+            "govt": 20,
+            "scty": 0
+        }
+    },
+    {
+        "name": "Socialism is when the state does stuff",
+        "stats": {
+            "econ": 70,
             "dipl": 50,
             "govt": 30,
             "scty": 50
         }
     },
     {
-        "name": "Neo-Conservatism",
+        "name": "Cattocomunista",
         "stats": {
-            "econ": 20,
-            "dipl": 20,
+            "econ": 100,
+            "dipl": 60,
             "govt": 40,
+            "scty": 40
+        }
+    },
+    {
+        "name": "Dorothy Day Fan",
+        "stats": {
+            "econ": 70,
+            "dipl": 100,
+            "govt": 100,
+            "scty": 40
+        }
+    },
+    {
+        "name": "Tumblr Socialism",
+        "stats": {
+            "econ": 60,
+            "dipl": 100,
+            "govt": 30,
+            "scty": 100
+        }
+    },
+    {
+        "name": "Ameriacommie",
+        "stats": {
+            "econ": 70,
+            "dipl": 20,
+            "govt": 50,
+            "scty": 70
+        }
+    },
+    {
+        "name": "REAL Libertarian",
+        "stats": {
+            "econ": 80,
+            "dipl": 80,
+            "govt": 80,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Unironic IWW Member",
+        "stats": {
+            "econ": 100,
+            "dipl": 50,
+            "govt": 80,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Berniebro",
+        "stats": {
+            "econ": 60,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 70
+        }
+    },
+    {
+        "name": "I Sware It's not Cathoic Socialism",
+        "stats": {
+            "econ": 50,
+            "dipl": 30,
+            "govt": 50,
+            "scty": 10
+        }
+    },
+    {
+        "name": "Co-op Capitalism",
+        "stats": {
+            "econ": 60,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Demorat",
+        "stats": {
+            "econ": 50,
+            "dipl": 100,
+            "govt": 30,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Euroboomer",
+        "stats": {
+            "econ": 60,
+            "dipl": 70,
+            "govt": 40,
+            "scty": 40
+        }
+    },
+    {
+        "name": "Social Fascist",
+        "stats": {
+            "econ": 60,
+            "dipl": 70,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "SJW",
+        "stats": {
+            "econ": 50,
+            "dipl": 100,
+            "govt": 20,
+            "scty": 100
+        }
+    },
+    {
+        "name": "Ancom but Pro Market and Sexist",
+        "stats": {
+            "econ": 60,
+            "dipl": 50,
+            "govt": 100,
             "scty": 20
         }
     },
     {
-        "name": "Fundamentalism",
+        "name": "1984",
         "stats": {
-            "econ": 20,
-            "dipl": 30,
-            "govt": 30,
-            "scty": 5
+            "econ": 50,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 80
         }
     },
     {
-        "name": "Libertarian Capitalism",
+        "name": "I HATE THE ANTI JESUS I HATE THE ANTI JESUS",
         "stats": {
-            "econ": 20,
+            "econ": 0,
+            "dipl": 100,
+            "govt": 0,
+            "scty": 100
+        }
+    },
+    {
+        "name": "Anti Market Science Nerd",
+        "stats": {
+            "econ": 100,
+            "dipl": 40,
+            "govt": 20,
+            "scty": 80
+        }
+    },
+    {
+        "name": "I Just Want to Grill for God Sake",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Libtard",
+        "stats": {
+            "econ": 50,
+            "dipl": 80,
+            "govt": 70,
+            "scty": 60
+        }
+    },
+    {
+        "name": "No Masters Only God/Gods",
+        "stats": {
+            "econ": 80,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Chud",
+        "stats": {
+            "econ": 30,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 20
+        }
+    },
+    {
+        "name": "RINO",
+        "stats": {
+            "econ": 30,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 40
+        }
+    },
+    {
+        "name": "Trav",
+        "stats": {
+            "econ": 50,
+            "dipl": 30,
+            "govt": 20,
+            "scty": 0
+        }
+    },
+    {
+        "name": "Redditor",
+        "stats": {
+            "econ": 60,
+            "dipl": 80,
+            "govt": 80,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Asian Wive Fan",
+        "stats": {
+            "econ": 30,
             "dipl": 50,
             "govt": 80,
             "scty": 60
         }
     },
     {
-        "name": "Market Anarchism",
+        "name": "Egotard",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 70
+        }
+    },
+    {
+        "name": "WHY THE FUCK ARE YOU THIS FUCK YOU NAZI",
+        "stats": {
+            "econ": 50,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "name": "Totalitarian Egotard",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 20,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Radical Socdem",
+        "stats": {
+            "econ": 60,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 40
+        }
+    },
+    {
+        "name": "+1000000000000 Social Points",
+        "stats": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Redneck",
+        "stats": {
+            "econ": 30,
+            "dipl": 40,
+            "govt": 50,
+            "scty": 30
+        }
+    },
+    {
+        "name": "Bogyman",
+        "stats": {
+            "econ": 20,
+            "dipl": 100,
+            "govt": 30,
+            "scty": 50
+        }
+    },
+    {
+        "name": "1776 BEST YEAR EVER",
+        "stats": {
+            "econ": 50,
+            "dipl": 20,
+            "govt": 80,
+            "scty": 80
+        }
+    },
+    {
+        "name": "commie genocide go burrrrr",
+        "stats": {
+            "econ": 0,
+            "dipl": 40,
+            "govt": 20,
+            "scty": 50
+        }
+    },
+    {
+        "name": "The Anti Wolff",
+        "stats": {
+            "econ": 40,
+            "dipl": 50,
+            "govt": 30,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Cove yourself in Oil",
+        "stats": {
+            "econ": 20,
+            "dipl": 80,
+            "govt": 30,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Sunni Jihad Warrior",
+        "stats": {
+            "econ": 50,
+            "dipl": 30,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "name": "I HATE THE IRS",
+        "stats": {
+            "econ": 0,
+            "dipl": 50,
+            "govt": 80,
+            "scty": 50
+        }
+    },
+    {
+        "name": "CRYPTO GO BURRRRRRR",
         "stats": {
             "econ": 20,
             "dipl": 50,
@@ -432,25 +432,25 @@ ideologies = [
         }
     },
     {
-        "name": "Totalitarian Capitalism",
+        "name": "COMMIE CHUCKER",
         "stats": {
             "econ": 0,
-            "dipl": 30,
+            "dipl": 0,
             "govt": 0,
             "scty": 50
         }
     },
     {
-        "name": "Ultra-Capitalism",
+        "name": "Mr Krabs",
         "stats": {
             "econ": 0,
-            "dipl": 40,
+            "dipl": 50,
             "govt": 50,
             "scty": 50
         }
     },
     {
-        "name": "Anarcho-Capitalism",
+        "name": "Drug Addict",
         "stats": {
             "econ": 0,
             "dipl": 50,
